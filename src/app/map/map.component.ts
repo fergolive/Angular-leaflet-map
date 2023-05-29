8e6787322ce3b2dffba37048df52bf431252a1f5
Fergolive <garciaolivo89@gmail.com>
2023-05-29 18:35:40 -0300

    style:install tailwind and get current position

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+
 
 @Component({
   selector: 'app-map',
@@ -11,63 +13,50 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   map:any;
+  positions:L.LatLng[]=[]
+  userLocation?:L.LatLng;
+  myIcon = L.divIcon({className: 'my-div-icon',iconSize:[50,90],bgPos:[0,0]});
 
-  private initMap(): void {
-
-    this.map = L.map('map').setView([-30.889332, -55.557966], 13);
-    
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
-  
-
-    var myIcon = L.divIcon({
-        className: 'my-div-icon',
-        iconSize:[50,90],
-        bgPos:[0,0]
-      });
-    
-// you can set .my-div-icon styles in CSS
-
-
-
-    this.map.on('click',  ($event:any)=> {
-      let lat=$event.latlng.lat;
-      let lng=$event.latlng.lng;
-    
-      L.marker([lat,lng],{draggable:true,icon: myIcon}).addTo(this.map)
-      
-    });
- 
-  }
-
-  constructor(
-
-  ) { 
-
-    
-
-
+  constructor() { 
+    this.getCurrentLocation()
   }
 
   ngAfterViewInit(): void {
     this.initMap();
-
-   
   }
 
-  addMarker($event:any){
-    console.log($event.latlng);
-
-    let lat=$event.latlng.lat;
-    let lng=$event.latlng.lng;
-
-    console.log(lat,lng);
-    
-    L.marker([lat,lng]).addTo(this.map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+  getCurrentLocation(){
+    navigator.geolocation.getCurrentPosition((data:any)=>{
+      let lat=data.coords.latitude;
+      let lng=data.coords.longitude;
+      this.userLocation=new L.LatLng(lat,lng);
+      this.addMarker(lat,lng)
+    })
   }
+
+  private initMap(): void {
+    //create map into id 'map'
+    this.map = L.map('map').setView([-30.889332, -55.557966], 16);
+    //setup tile layer (map style) - see documentation to change style of map
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    //event click of map to add marker
+    this.map.on('click',  ($event:any)=> {
+      let lat=$event.latlng.lat;
+      let lng=$event.latlng.lng;
+      this.addMarker(lat,lng)
+    });
+  }
+
+  addMarker(lat:any,lng:any){
+    L.marker([lat,lng],{draggable:true,icon: this.myIcon}).addTo(this.map)
+  }
+
+
+
+
+
 
  
 
